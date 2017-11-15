83d953137ef19b73a669849f247a486601722b20
MSI\Lucas <lucasblon@orange.fr>
2017-11-15 11:27:20 +0100

    on es pas des codeurs nous ?

---
+++ Assets/script/DestructibleObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObjects : MonoBehaviour, ITakeDamage
+{
+    public float MaxHealth = 100f;
+    private float _currentHealth = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _currentHealth = MaxHealth;
+	}
+
+    public void TakeDamage(float damage, GameObject instigator)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+            Kill();
+    }
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+}
