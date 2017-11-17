4fbd5a278603d2707292a5ebcbb6e629c1932c60
Mikael.PC <mikaeljouchoux@gmail.com>
2017-11-17 12:01:22 +0100

    UI

---
+++ Assets/script/HUDHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; 
+
+public class HUDHandler : MonoBehaviour
+{
+
+
+    [Header("Game")]
+    public Pawn Player;
+
+    [Header("Interface")]
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI AmmoText;
+    public Image HealtBar;
+    // Use this for initialization
+    void Awake ()
+    {
+        Assert.IsNotNull(Player);
+        Assert.IsNotNull(ScoreText);
+        Assert.IsNotNull(AmmoText);
+        Assert.IsNotNull(HealtBar);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Player != null)
+        {
+            AmmoText.text = "x " + Player.Ammo;
+        }	
+	}
+}
