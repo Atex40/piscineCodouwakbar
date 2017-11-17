f4d0b4a1975bad28086b24f349ff092415dd8653
MSI\Lucas <lucasblon@orange.fr>
2017-11-17 09:32:26 +0100

    Bonus

---
+++ Assets/script/Bonus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public abstract class Bonus : MonoBehaviour {
+
+	// Use this for initialization
+	private void OnTriggerEnter (Collider other)
+    {
+        Pawn pawn = other.gameObject.GetComponentInParent<Pawn>();
+        if (pawn != null)
+        {
+            ApplyBonus();
+        }
+		
+	}
+	
+	// Update is called once per frame
+	public void virtual ApplyBonus ()
+    {
+		
+	}
+}
