719fdda16708a6d54fc52c5dc57417d581af52f5
MSI\Lucas <lucasblon@orange.fr>
2017-11-23 11:00:10 +0100

    main menu

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button Playbtn;
+    public Button Quitbtn;
+
+    // Use this for initialization
+    void Start()
+    {
+        Navigation navigation = Playbtn.navigation;
+
+        navigation.mode = Navigation.Mode.Explicit;
+
+        navigation.selectOnDown = Quitbtn;
+
+        Playbtn.navigation = navigation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Scene Kevin");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+}
