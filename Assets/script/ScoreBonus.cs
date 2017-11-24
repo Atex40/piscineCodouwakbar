8a04c0968d17ce5ec584ffd4c9f7b797a394a6b9
MSI\Lucas <lucasblon@orange.fr>
2017-11-24 12:05:54 +0100

    qqqqqq

--- Assets/script/ScoreBonus.cs
+++ Assets/script/ScoreBonus.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ScoreBonus : Bonus
 {
-
+    private bool destroy = false;
 	public int Score = 50;
     
     public override void ApplyBonus (Pawn pawn)
     {
         pawn.AddScore(Score);
         _animator.SetTrigger("Pickup");
+        destroy = true;
+        if (destroy == true)
+        {
+           Destroy(gameObject);
+        }
     }
 
+    
 }
